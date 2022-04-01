42b40facc0c87dde20246ec2d9990630a37875c6
Robert Bastian <robertbastian@users.noreply.github.com>
2022-04-01 07:16:03 +0200

    Update captcha_check.js

--- captcha_check.js
+++ captcha_check.js
@@ -1,22 +1,23 @@
-// webtask.io task
+// GCP Cloud Function
 //
-// Secrets:  'recaptcha', the reCaptcha key
-//           'payload', the string to be sent
-// URL args: 'response', the captcha's response token
+// Environment vars:
+// process.env.SECRET: the reCaptcha key
+// process.env.PAYLOAD: the string to be sent
 
-module.exports = function(context, req, res){
-  require('request@2.56.0')(
+exports.main = function(req, res){
+  res.set('Access-Control-Allow-Origin', "*")
+  res.set('Access-Control-Allow-Methods', 'GET, POST');
+  require('request')(
     'https://www.google.com/recaptcha/api/siteverify' +
-      '?secret=' + context.secrets.recaptcha +
-      '&response=' + context.query.response,
-    function(error, res2, body){
-      if (error)
-        res.writeHead(500, "Error validating captcha response")
-      else if (!JSON.parse(body).success)
-        res.writeHead(403,"Invalid captcha response")
-      else
-        res.write(context.secrets.payload)
-      res.end()
+    '?secret=' + process.env.SECRET + '&response=' + req.query.response,
+    (e, _, b) => {
+      if (e) {
+        res.writeHead(500, "Error validating captcha response");
+      } else if (!JSON.parse(b).success) {
+        res.writeHead(403,"Invalid captcha response");
+      } else {
+        res.write(process.env.PAYLOAD);
+      res.end();
     }
-  )
-}
+  });
+};
